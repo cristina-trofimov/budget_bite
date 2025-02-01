0354883bfe63ec7b5b359a97acfe9015f50062a4
j-ackamir <ja_amir@live.concordia.ca>
2025-02-01 17:35:23 -0500

    Metro parser

--- flyers/metroparser/parser.py
+++ flyers/metroparser/parser.py
@@ -10,10 +10,19 @@ pattern = r'alt="([^"]+)"'
 # Find all matches
 matches = re.findall(pattern, content, re.DOTALL)
 
-
-# Write the matches to a new file
+# Remove all lines smaller than 3 words
+for match in matches:
+    if len (match.split())<=2:
+        matches.remove(match)
+        
 with open('output.txt', 'w') as output_file:
     for match in matches:
-        output_file.write(match + '\n')
+        first_part = re.search(r'^([^.]*)\.', match)
+        last_part = re.search(r'\.?\s*([\d,]+\.\d+)', match)
+        
+        if first_part and last_part:
+            combined = f"{first_part.group(1)} {last_part.group(1)}"
+            output_file.write(combined + '\n')
+        
 
 print("Extraction complete. Check 'output.txt' for the results.")
