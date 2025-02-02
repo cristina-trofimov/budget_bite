89190fc3051e0a0370b042e0f8e660e9b9f7bbd3
cristina <cristina.trofimov03@gmail.com>
2025-02-02 03:16:08 -0500

    home page is done

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Text } from '@mantine/core';
+import './HomePage.css';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+
+  const navigate = useNavigate();
+
+
   return (
-    <div>HomePage</div>
+    <div className="homeStack">
+      <div className="headline shrikhand_regular">
+        Turn flyer specials into special meals
+      </div>
+      <div className="stores">
+        <div className="stores_image" onClick={() => navigate(`/recipe/metro`)}>
+          <img src="/metro.png" alt="Metro" />
+        </div>
+        <div className="stores_image" onClick={() => navigate(`/recipe/maxi`)}>
+          <img src="/maxi.png" alt="Maxi" />
+        </div>
+        <div className="stores_image" onClick={() => navigate(`/recipe/superC`)}>
+          <img src="/superC.png" alt="Super C" />
+        </div>
+        <div className="stores_image" onClick={() => navigate(`/recipe/iga`)}>
+          <img src="/iga.png" alt="IGA" />
+        </div>
+      </div>
+    </div>
   )
 }
 
